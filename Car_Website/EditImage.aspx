650f232b987184a3b95dca8446e07679251fb7ef
david pheng <phengdavid979@gmail.com>
2022-12-11 14:38:14 +0700

    finish admin code

---
+++ Car_Website/EditImage.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/AdminMasterPage.master" AutoEventWireup="true" CodeFile="EditImage.aspx.cs" Inherits="EditImage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="AdminMasterPagePlaceholder" Runat="Server">
+    <div>
+            <h2>Search For The Watch By ID To Edit The Watch Image </h2>
+        <table style="width:90%">
+        <tr>
+            <td colspan="3" style="text-align:center">
+                <asp:Label runat="server" ID="lblMes" ForeColor="#CC3300" Font-Bold="True" 
+                    Font-Italic="True" BackColor="#FFFFCC"></asp:Label></td>
+        </tr>
+        <tr style="vertical-align:top">
+            <td style="width:20%; padding:15px; vertical-align:top;">Watch ID</td>
+            <td style="width:25%; vertical-align:top;"> 
+                <asp:TextBox runat="server" ID="cId" Width="180"></asp:TextBox>
+                <br /><br />
+                <asp:Button runat="server" ID="btnSearch" Text="Search" OnClick="btnSearch_Click"  /> 
+            </td>
+   
+            <td style="width:50%; vertical-align:top; text-align:center"> 
+                The Main Image <br />
+                <asp:ListView runat="server" ID="lview1" >
+                     <ItemTemplate>
+                        <asp:image Width="300px" ID="mImgUrl" runat="server" 
+                            ImageUrl='<%# Eval("ImgUrl") %>' />
+                        <br />
+                        <asp:textbox Visible="false" ID="mImgwId" runat="server" 
+                            Text='<%# Eval("cId") %>' />
+                    </ItemTemplate>
+                </asp:ListView> 
+            </td>
+         </tr>
+        <tr>
+            <td colspan="3" style="text-align:center">
+                Choose The New Main Image : 
+                <asp:FileUpload ID="fmImg" runat="server" />
+                <asp:Button runat="server" Text="Update the Main Image" ID="cmdUMI"  OnClick="cmdUMI_Click"
+                    />
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="3">
+                 <hr />
+                <h3>More Images</h3>
+       <asp:ListView ID="LView2" runat="server" DataKeyNames="ImgId"  GroupItemCount="3"   OnItemCommand="LView2_ItemCommand"
+         >
+           <ItemTemplate>
+            <td id="Td1" runat="server" style="padding:10px; text-align:center">
+                <asp:TextBox Visible="False" ID="txtImgIds" runat="server" 
+                    Text='<%# Eval("ImgId") %>' />
+                <asp:TextBox Visible="False" ID="TextBox1" runat="server" 
+                    Text='<%# Eval("cId") %>' />
+                <br />
+                <asp:Image ImageAlign="Middle" ID="Image2" runat="server" Width="220" 
+                    ImageUrl='<%# Eval("ImgUrls") %>' />
+                <br /><br />
+                 <asp:LinkButton CommandName="ImgDel" ID="ImgDel" Text="Delete" runat="server" 
+                     Font-Bold="false" ForeColor="#009999" Font-Italic="true"></asp:LinkButton>
+                <br />
+            </td>
+        </ItemTemplate>
+ 
+        <GroupTemplate>
+            <tr id="itemPlaceholderContainer" runat="server">
+                <td id="itemPlaceholder" runat="server"></td>
+            </tr>
+        </GroupTemplate>
+        <LayoutTemplate>
+            <table id="Table1" runat="server">
+                <tr id="Tr1" runat="server">
+                    <td id="Td2" runat="server">
+                        <table id="groupPlaceholderContainer" runat="server" border="0" style="">
+                            <tr id="groupPlaceholder" runat="server">
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+                <tr id="Tr2" runat="server">
+                    <td id="Td3" runat="server" style="">
+                    </td>
+                </tr>
+            </table>
+        </LayoutTemplate>
+     </asp:ListView>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3" style="text-align:center">
+                Choose New Image to Upload : 
+                <asp:FileUpload ID="fmImg2" runat="server" />
+                <asp:Button runat="server" ID="ImgUpload" Text="Upload New Image" OnClick="ImgUpload_Click"
+                   />
+            </td>
+        </tr>
+     </table>
+    </div>
+</asp:Content>
+
+
+
+
---
+++ Car_Website/EditImage.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class EditImage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+
+        SqlConnection cnn = new SqlConnection("Data Source=DESKTOP-LBEC58U;Initial Catalog=carDb;Integrated Security=True");
+
+        SqlCommand cmd = new SqlCommand("Select cId, ImgUrl From tblCar where cId=@cId", cnn);
+        cmd.Parameters.Add("@cId", System.Data.SqlDbType.NVarChar);
+        cmd.Parameters["@cId"].Value = cId.Text;
+
+        SqlCommand cmd1 = new SqlCommand("Select * From tblImages where cId=@cId", cnn);
+        cmd1.Parameters.Add("@cId", System.Data.SqlDbType.NVarChar);
+        cmd1.Parameters["@cId"].Value = cId.Text;
+
+        SqlDataReader rd, rd1;
+        cnn.Open();
+        rd = cmd.ExecuteReader();
+        lview1.DataSource = rd;
+        lview1.DataBind();
+        rd.Close();
+        cnn.Close();
+
+        cnn.Open();
+        rd1 = cmd1.ExecuteReader();
+        LView2.DataSource = rd1;
+        LView2.DataBind();
+        rd1.Close();
+        cnn.Close();
+
+    }
+
+    protected void cmdUMI_Click(object sender, EventArgs e)
+    {
+        if (fmImg.HasFile)
+        {
+            String imgName = fmImg.FileName;
+            fmImg.SaveAs(Server.MapPath("~\\Images") + "/" + fmImg.FileName);
+
+            SqlConnection cnn = new SqlConnection("Data Source=DESKTOP-LBEC58U;Initial Catalog=carDb;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Update tblCar Set ImgUrl=@ImgUrl " +
+                "Where cId=@cId", cnn);
+
+            cmd.Parameters.Add("@cId", System.Data.SqlDbType.NVarChar);
+            cmd.Parameters["@cId"].Value = cId.Text;
+            cmd.Parameters.Add("@ImgUrl", System.Data.SqlDbType.NVarChar);
+            cmd.Parameters["@ImgUrl"].Value = "Images/" + imgName;
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+
+            SqlCommand cmd1 = new SqlCommand("Select cId, ImgUrl From tblCar " +
+                "where cId=@cId", cnn);
+
+            cmd1.Parameters.Add("@cId", System.Data.SqlDbType.NVarChar);
+            cmd1.Parameters["@cId"].Value = cId.Text;
+
+            SqlDataReader rd1;
+            cnn.Open();
+            rd1 = cmd1.ExecuteReader();
+            lview1.DataSource = rd1;
+            lview1.DataBind();
+            rd1.Close();
+            cnn.Close();
+        }
+    }
+
+    protected void ImgUpload_Click(object sender, EventArgs e)
+    {
+        if (fmImg2.HasFile)
+        {
+            String imgName2 = fmImg2.FileName;
+            fmImg2.SaveAs(Server.MapPath("~\\Images") + "/" + fmImg2.FileName);
+
+            SqlConnection cnn = new SqlConnection("Data Source=DESKTOP-LBEC58U;Initial Catalog=carDb;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("Insert Into tblImages Values(@cId, @ImgUrls)", cnn);
+            cmd.Parameters.Add("@cId", System.Data.SqlDbType.NVarChar);
+            cmd.Parameters["@cId"].Value = cId.Text;
+            cmd.Parameters.Add("@ImgUrls", System.Data.SqlDbType.NVarChar);
+            cmd.Parameters["@ImgUrls"].Value = "Images/" + imgName2;
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+            SqlCommand cmd1 = new SqlCommand("Select * From tblImages where cId=@cId", cnn);
+            cmd1.Parameters.Add("@cId", System.Data.SqlDbType.NVarChar);
+            cmd1.Parameters["@cId"].Value = cId.Text;
+            SqlDataReader rd1;
+            cnn.Open();
+            rd1 = cmd1.ExecuteReader();
+            LView2.DataSource = rd1;
+            LView2.DataBind();
+            rd1.Close();
+            cnn.Close();
+        }
+
+    }
+
+
+
+    protected void LView2_ItemCommand(object sender, ListViewCommandEventArgs e)
+    {
+        TextBox tIdD = (TextBox)e.Item.FindControl("txtImgIds");
+        SqlConnection cnn = new SqlConnection("Data Source=DESKTOP-LBEC58U;Initial Catalog=carDb;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand("Delete From tblImages Where ImgId=@ImgIdD", cnn);
+        cmd.Parameters.Add("@ImgIdD", System.Data.SqlDbType.Int);
+        cmd.Parameters["@ImgIdD"].Value = tIdD.Text;
+        cnn.Open();
+        cmd.ExecuteNonQuery();
+        cnn.Close();
+        //Showing after Deleting
+        SqlCommand cmd1 = new SqlCommand("Select * From tblImages where cId=@cId", cnn);
+        cmd1.Parameters.Add("@cId", System.Data.SqlDbType.NVarChar);
+        cmd1.Parameters["@cId"].Value = cId.Text;
+        SqlDataReader rd1;
+        cnn.Open();
+        rd1 = cmd1.ExecuteReader();
+        LView2.DataSource = rd1;
+        LView2.DataBind();
+        rd1.Close();
+        cnn.Close();
+    }
+
+}
